46fc6597eb5401e000388f08d6f48f0870fe1808
Graham Otte <graham.otte@appfolio.com>
2019-03-31 16:15:02 -0700

    elixir

---
+++ conway.ex
@@ -0,0 +1,81 @@
+defmodule Conway do
+  def new_board(size) do
+    0..size |> Enum.map(fn _ ->
+      0..size |> Enum.map(fn _ ->
+        Enum.random(0..1) == 1
+      end)
+    end)
+  end
+
+  def tick(board) do
+    board |> Enum.with_index |> Enum.map(fn row ->
+      { row_values, row_index } = row
+
+      row_values |> Enum.with_index |> Enum.map(fn col ->
+        { cell, col_index } = col
+        num_neighbors = live_neighbors(board, row_index, col_index)
+
+        case cell do
+          true -> case num_neighbors do
+            2 -> true
+            3 -> true
+            _ -> false
+          end
+          false -> case num_neighbors do
+            3 -> true
+            _ -> false
+          end
+        end
+      end)
+    end)
+  end
+
+  defp live_neighbors(board, row, col) do
+    Enum.count([
+      get_val(board, row, col + 1),
+      get_val(board, row, col - 1),
+      get_val(board, row + 1, col),
+      get_val(board, row - 1, col),
+      get_val(board, row + 1, col + 1),
+      get_val(board, row - 1, col - 1),
+      get_val(board, row + 1, col - 1),
+      get_val(board, row - 1, col + 1),
+    ], &(&1))
+  end
+
+  defp get_val(board, row, col) do
+    row = rem(row, length(board))
+    col = rem(col, length(board))
+
+    try do
+      board |> Enum.at(row) |> Enum.at(col)
+    rescue
+      _ -> nil
+    end
+  end
+
+  def to_s(board) do
+    board |> Enum.map(fn row ->
+      row |> Enum.map(fn i ->
+        if i do
+          '#'
+        else
+          '.'
+        end
+      end)
+      |> Enum.join(" ")
+    end)
+    |> Enum.join("\n")
+  end
+end
+
+board = Conway.new_board(20)
+
+Enum.reduce(1..100, board, fn (_, board) ->
+  IO.puts Conway.to_s(board)
+  IO.puts ''
+
+  Process.sleep(100)
+
+  board |> Conway.tick
+end)
