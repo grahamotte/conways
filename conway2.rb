bb1991f121e5739e83088d8992c4db5bb6fb8f5b
Graham Otte <go@grahamotte.com>
2022-06-19 20:43:52 -0700

    Arrays are faster

--- conway2.rb
+++ conway2.rb
@@ -1,84 +1,47 @@
 # ruby conway2.rb
 
-class Board
-  attr_accessor :data, :size
+class Conway
+  attr_accessor :board, :size
 
   def initialize(size)
     @size = size
-    @data = Hash.new { false }
-  end
-
-  def get(r, c)
-    @data[[r % size, c % size]]
-  end
-
-  def set(r, c)
-    @data[[r % size, c % size]] = true
-  end
-
-  def active_locations
-    @data.keys + @data.keys.map { |r, c| adj(r, c) }.flatten(1)
-  end
-
-  def neighbors(r, c)
-    adj(r, c).map { |r, c| get(r, c) }.count { |x| x }
-  end
-
-  def to_s
-    size.times.map do |r|
-      size.times.map do |c|
-        get(r, c) ? '#' : '.'
-      end.join(' ')
-    end.join("\n")
-  end
-
-  private
-
-  def adj(r, c)
-    [
-      [r + 0, c + 1],
-      [r + 0, c - 1],
-      [r + 1, c + 0],
-      [r - 1, c + 0],
-      [r + 1, c + 1],
-      [r - 1, c - 1],
-      [r + 1, c - 1],
-      [r - 1, c + 1],
-    ]
-  end
-end
-
-class Conway
-  attr_accessor :board
-
-  def initialize(size)
-    @board = Board.new(size)
-
-    (size * size / 2).times { board.set(rand(0..size), rand(0..size)) }
+    @board = (0..(size - 1)).map { (0..(size - 1)).map { [true, false].sample } }
   end
 
   def tick
-    nb = Board.new(board.size)
+    @board = begin
+      board.map.with_index do |row, r|
+        row.map.with_index do |col, c|
+          count = 0
+          count += 1 if board[(r + 0) % size][(c + 1) % size]
+          count += 1 if board[(r + 0) % size][(c - 1) % size]
+          count += 1 if board[(r + 1) % size][(c + 0) % size]
+          count += 1 if board[(r - 1) % size][(c + 0) % size]
+          count += 1 if board[(r + 1) % size][(c + 1) % size]
+          count += 1 if board[(r - 1) % size][(c - 1) % size]
+          count += 1 if board[(r + 1) % size][(c - 1) % size]
+          count += 1 if board[(r - 1) % size][(c + 1) % size]
 
-    board.active_locations.each do |r, c|
-      neih = board.neighbors(r, c)
-      cell = board.get(r, c)
-      nb.set(r, c)if neih == 3 || (neih == 2 && cell)
+          count == 3 || (col && count == 2)
+        end
+      end
     end
-
-    @board = nb
   end
 
   def to_s
-    board.to_s
+    board.map do |row|
+      row.map do |col|
+        col ? '#' : '.'
+      end.join(' ')
+    end.join("\n")
   end
 end
 
-s = 50
-c = Conway.new(s)
+size = 50
+conway = Conway.new(size)
 
 100.times do
-  print "\033[#{s}A"
-  puts c
-  c.tick
+  print "\033[#{size}A"
+  puts conway
+  conway.tick
 end
