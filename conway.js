4e4d3c3440b529f182de11ba2ee863618bbd36c4
Graham Otte <go@grahamotte.com>
2022-08-06 23:11:08 +0000

    Update js

--- conway.js
+++ conway.js
@@ -1,76 +1,42 @@
 // node conway.js
 
-const range = (n) => {
-  return [...Array(n).keys()];
-};
+const size = 50;
 
-const randRange = (max) => Math.floor(Math.random() * (max + 1));
+const range = (n) => [...Array(n).keys()];
 
-const boardToString = (board) => {
-  return board
-    .map((row) => {
-      return row
-        .map((col) => {
-          return col ? "#" : ".";
-        })
-        .join(" ");
-    })
-    .join("\n");
-};
+const mod = (n, m) => ((n % m) + m) % m;
 
-const liveOrDie = (board, row, col) => {
-  countNeighbors = [
-    board[mod(row + 0, board.length)][mod(col + 1, board.length)],
-    board[mod(row + 0, board.length)][mod(col - 1, board.length)],
-    board[mod(row + 1, board.length)][mod(col + 0, board.length)],
-    board[mod(row - 1, board.length)][mod(col + 0, board.length)],
-    board[mod(row + 1, board.length)][mod(col + 1, board.length)],
-    board[mod(row - 1, board.length)][mod(col - 1, board.length)],
-    board[mod(row + 1, board.length)][mod(col - 1, board.length)],
-    board[mod(row - 1, board.length)][mod(col + 1, board.length)],
-  ].filter((x) => x).length;
+var board = range(size).map((row) => {
+  return range(size).map((col) => {
+    return Math.random() < 0.5;
+  });
+});
 
-  if (board[row][col]) {
-    return countNeighbors == 2 || countNeighbors == 3;
-  } else {
-    return countNeighbors == 3;
-  }
-};
-
-const mod = (a, b) => {
-  const r = a % b;
-  return r < 0 ? r + b : r;
-};
-
+var count;
 const tick = (board) => {
-  return board.map((row, row_index) => {
-    return row.map((cell, col_index) => {
-      return liveOrDie(board, row_index, col_index);
+  return board.map((row, r) => {
+    return row.map((cell, c) => {
+      count = 0;
+
+      if (board[mod(r + 0, size)][mod(c + 1, size)]) count += 1;
+      if (board[mod(r + 0, size)][mod(c - 1, size)]) count += 1;
+      if (board[mod(r + 1, size)][mod(c + 0, size)]) count += 1;
+      if (board[mod(r - 1, size)][mod(c + 0, size)]) count += 1;
+      if (board[mod(r + 1, size)][mod(c + 1, size)]) count += 1;
+      if (board[mod(r - 1, size)][mod(c - 1, size)]) count += 1;
+      if (board[mod(r + 1, size)][mod(c - 1, size)]) count += 1;
+      if (board[mod(r - 1, size)][mod(c + 1, size)]) count += 1;
+
+      return count == 3 || (cell && count == 2);
     });
   });
 };
 
-function sleep(ms, callback) {
-  var start = new Date().getTime();
-  var expire = start + ms;
+const toS = (b) =>
+  b.map((r) => r.map((c) => (c ? "●" : "·")).join(" ")).join("\n");
 
-  while (new Date().getTime() < expire) {}
-
-  callback(ms);
-}
-
-const size = 20;
-
-var board = range(size).map((row) => {
-  return range(size).map((col) => {
-    return randRange(1) == 1;
-  });
-});
-
-range(100).forEach((acc, val) => {
-  sleep(100, () => {
-    console.log(boardToString(board));
-    console.log("");
-    board = tick(board);
-  });
+range(1000).forEach(() => {
+  board = tick(board);
+  process.stdout.write("\033" + `[${size}A`);
+  console.log(toS(board));
 });
